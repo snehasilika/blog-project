954cbff22dd60ff0a057a039aad38bd854e736d1
snehasilika <35603358+snehasilika@users.noreply.github.com>
2020-08-20 20:55:34 +0530

    Latest and Most commented post

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,17 @@
+from django import template
+from blog.models import Post
+from django.db.models import Count
+register=template.Library()
+
+@register.simple_tag(name='my_tag')
+def total_posts():
+  return Post.objects.count() 
+
+@register.inclusion_tag('latest_posts123.html')
+def show_latest_posts(count=5):
+  latest_posts=Post.objects.order_by('-publish')[:count]
+  return {'latest_posts':latest_posts}
+
+@register.simple_tag
+def get_most_commented_posts(count=3):
+  return Post.objects.annotate(total_comments=Count('comments')).order_by('total_comments')[:count] 
